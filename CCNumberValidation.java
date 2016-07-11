4abaaf42fc368872282b7030a92d6abb9652bbbc
leetompkins <letompki@gmail.com>
2016-07-11 13:43:41 -0500

    Updated CCNumberValidation
    CCNumberValidation would not return correct validity, now returns
    correct validity

--- CCNumberValidation.java
+++ CCNumberValidation.java
@@ -1,19 +1,18 @@
 /* Week3 Chapter6 Exercise 6.31
- * Date Jul 8 2016
+ * Date Jul 8 2016, Last Modified Jul 11 2016
  * Author Lee Tompkins 
  * File CCNumberValidation.java
  * Summary: A program that checks whether a credit card is valid or not valid 
  * with the use of multiple pre-defined methods
  */
-
 import java.util.Scanner;
 public class CCNumberValidation {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); // Create Scanner
 		System.out.print("Enter in a CC number to determine if it is valid or not");
-		long ccNumber = input.nextLong();
-		if(CCNumValidationMethods.isValid(ccNumber)) {
+		long ccNumber = input.nextLong(); // Store CC input as long 
+		if(CCNumValidationMethods.isValid(ccNumber)) { // Display if CC num is valid or not
 			System.out.println(ccNumber + " Is a valid card number");
 		} else {
 			System.out.println(ccNumber + " Is not a valid card number");
@@ -27,83 +26,88 @@ public class CCNumberValidation {
 class CCNumValidationMethods {
 	//Return true if card num valid
 	public static boolean isValid(long number){
-		
-		long x = number;
-		String xD = Long.toString(x);
-		xD = xD.substring(0, 1);
-		int d = Integer.valueOf(xD);
+		// Declare variables an booleans 
+		int d =0;
+		boolean sum = false;
 		boolean valid = false;
-		boolean isValidLength = false;
-		boolean prefixValid = false;
-		boolean sumTotal = false;
-		if(CCNumValidationMethods.getDigit((int) number) == 75) {
-			sumTotal = true;
+		boolean prefixMatched = false;
+		boolean length = false;
+		// Set booleans based on method returns 
+		if ((CCNumValidationMethods.sumOfDoubleEvenPlace(number) + CCNumValidationMethods.sumOfOddPlace(number)) % 10 == 0) {
+			sum = true;
+		} else {
+			sum = false;
 		}
-		if(13 <= CCNumValidationMethods.getSize(number) && CCNumValidationMethods.getSize(number) <= 16) {
-			isValidLength = true;
+		if (CCNumValidationMethods.prefixMatched(number, d)) {
+			prefixMatched = true;
+		} else {
+			prefixMatched = false;
 		}
-		if(CCNumValidationMethods.prefixMatched(number, d)){
-			prefixValid = true;
+		if(CCNumValidationMethods.getSize(number) >= 13 && CCNumValidationMethods.getSize(number) <= 16) {
+			length = true;
+		} else {
+			length = false;
 		}
-		if(isValidLength && prefixValid && sumTotal) {
+		if(sum && prefixMatched && length) {
 			valid = true;
+		} else {
+			valid = false;
 		}
-		return valid;
+		return valid; // return final validity
 	}
+	
 	//Get result from step 2
 	public static int sumOfDoubleEvenPlace(long number){
-		int k = 10;
-		long x = CCNumValidationMethods.getPrefix(number, k);
-		int xX = (int) x;
-		return xX;
+		String longNumber = Long.toString(number); // convert long to string
+		int sumOfDoubleEven = 0; // initialize variable for double sums
+		for(int i = 0; i < longNumber.length(); i += 2 ) {
+			sumOfDoubleEven += CCNumValidationMethods.getDigit(longNumber.charAt(i) - 48);
+		}
+		return sumOfDoubleEven;
 	}
 		
 	// Return this number if it is a single digit or
 	//return sum if two digits
 	public static int getDigit(int number) {
-		int sum = CCNumValidationMethods.sumOfDoubleEvenPlace(number) + CCNumValidationMethods.sumOfOddPlace(number) ; 
-		return sum;
+		number = number * 2;
+		if(number < 10) {
+			return number;
+		} else {
+			String strNumber = Integer.toString(number);
+			int sum = (strNumber.charAt(0) - 48) + (strNumber.charAt(1) - 48);
+			return sum;
+		}
 	}
 	// Return sum of odd placed digits in number
 	public static int sumOfOddPlace(long number) {
 		 String strNumber = Long.toString(number);
-		 int sum = strNumber.charAt(1);
+		 int sum = strNumber.charAt(1) - 48;
 		for(int i = 3; i <= strNumber.length(); i += 2) {
-			sum += strNumber.charAt(i);
+			sum = sum + (strNumber.charAt(i) - 48);
 		}
 		return sum;
 	}
 	// Return true if digit d is prefix for number
 	public static boolean prefixMatched(long number, int d) {
-		String x = Integer.toString(d);
-		if(x.charAt(0) == 4 || x.charAt(0) == 5 || x.charAt(0) == 6) {
+		String x = Long.toString(number);
+		int xx = x.charAt(0) - 48;
+		if(xx == 4 || xx == 5 || xx == 6) {
 			return true;
-		} else if (x.charAt(0) == 3 && x.charAt(1) == 7) {
+		} else if ((x.charAt(d) - 48) == 3 && (x.charAt(1) - 48) == 7) {
 			return true;
-		} else return false;
+		} else {
+			return false;
+		}
 	}
 	// Return the number of digits in d
 	public static int getSize(long d) {
-		int length;
-		String strLength = Long.toString(d);
-		length = strLength.length();
-		return length;
+		int length; // Declare variable length
+		String strLength = Long.toString(d); // convert to string
+		length = strLength.length(); // set length equal to string length
+		return length; // return length
 	}
 	// Return the first k number of digits from number. If the number of 
 	// digits in number is less than k return number
-	public static long getPrefix(long number, int k) {
-		String strNumber = Long.toString(number);
-		int sum = 0;
-		for (int i = strNumber.length(); i <= 0; i -=2) {
-			if (strNumber.charAt(i) * 2 >= k) {
-				int x = strNumber.charAt(i) * 2;
-				String xX = Integer.toString(x);
-				int y = xX.charAt(0) + xX.charAt(1);
-				sum += y; 
-			} else {
-				sum += strNumber.charAt(i) * 2;
-			}
-		}
-		return sum;
-	}
+	//public static long getPrefix(long number, int k) {
+	//}	
 }
